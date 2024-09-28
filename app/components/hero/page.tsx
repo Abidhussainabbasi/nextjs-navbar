99d8bb132bec0ab689a83282d10f45716f5dbaf9
unknown <abidhussaink640gmail.com>
2024-09-28 09:50:28 +0500

    first commit

---
+++ app/components/hero/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='bg-green-400 h-screen w-[100%]'>
+        Hero
+    </div>
+  )
+}
+
+export default Hero
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "./components/hero/page";
+import Navbar from "./components/navbar/page";
+export default function Myname(){
+
+return <div>
+  <Navbar/>
+  <Hero/>
+</div>
+
+}
